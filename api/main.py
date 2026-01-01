0065a52445fbe938709e9f8d1ad2da0fd741a156
srinjoy-zeron <srinjoy@zeron.one>
2026-01-02 01:51:46 +0530

    prod changes

--- api/main.py
+++ api/main.py
@@ -6,6 +6,8 @@ logging.basicConfig(filename = "logs.log", level=logging.INFO, format='%(asctime
 logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    uvicorn.run("hses:app", host = "127.0.0.1", port = 8000, reload = True)
+# use this below only in local dev
+# uvicorn api.app:app --env-file api/.env --reload -> run this command in project root to start the server
+# if __name__ == "__main__":
+#     uvicorn.run("hses:app", host = "127.0.0.1", port = 8000, reload = True)
 
